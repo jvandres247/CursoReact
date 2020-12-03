037f0738591d02c196ee6c9808f1460c293d0838
Andres Velazquez <jvandres247@gmail.com>
2020-12-02 22:59:37 -0600

    finish gif app and generate build for deploy

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  return (
+    <div>
+      <h2>Gif App Search</h2>
+      <AddCategory setCategories={setCategories}/>
+      <hr />
+      <ol>
+        {categories.map((category) => (
+            <GifGrid key={category} category={category}/>
+        ))}
+      </ol>
+    </div>
+  );
+};
