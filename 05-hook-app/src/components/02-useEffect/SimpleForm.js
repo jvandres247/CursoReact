7618dfe132522953a6c75104ea8ccaa398821bdc
Andres Velazquez <jvandres247@gmail.com>
2020-12-07 13:17:23 -0600

    Video 110 from course, hooks

---
+++ 05-hook-app/src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import "./effects.css";
+import {Message} from './Message';
+
+
+export const SimpleForm = () => {
+  const [formState, setFormStare] = useState({
+    name: "",
+    email: "",
+  });
+
+  const { name, email } = formState;
+
+  useEffect(() => {
+    console.log("Hey!");
+  },[]);
+
+  useEffect(() => {
+    console.log("formState cambio");
+  },[formState]);
+
+  useEffect(() => {
+    console.log("email cambio");
+  },[email]);
+
+
+  const handleInputChange = ({target}) => {
+    setFormStare({
+        ...formState,
+        [target.name]: target.value
+    })
+  };
+
+  return (
+    <>
+      <h1>useEffect</h1>
+      <hr />
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="Correo Electronico"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      {name === '123' && <Message/>}
+    </>
+  );
+};
